51e54793e894a93eec937ef023a93e1701e181de
fapethedev <mpetrivlin@gmail.com>
2024-07-01 11:17:58 +0000

    Learn NextJs with Vercel :  CSS Styling   - Global Styling

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,14 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div
+              className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+          />
           <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6"/>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
