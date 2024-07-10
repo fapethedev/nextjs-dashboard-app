67cb86c97a1994ab8fb41c500fe3e63767a043a1
fapethedev <mpetrivlin@gmail.com>
2024-07-10 11:56:10 +0000

    Learn NextJs with Vercel:  Mutating Data:   - Updating an invoice:     4-Pass the id to the Server Action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,6 +20,9 @@ const CreateInvoice = FormSchema.omit(
     }
 );
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const {
       customerId,
@@ -37,6 +40,25 @@ export async function createInvoice(formData: FormData) {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
 
   revalidatePath('/dashboard/invoices');
